ac252463d9c5521d804f5c0c1093f66492ddac9e
Sai Sanjay Kottakota <97831658+sanjay7178@users.noreply.github.com>
2023-08-13 23:06:47 +0530

    Fixed Bugs

---
+++ login.php
@@ -0,0 +1,87 @@
+<?php
+$login  = false;
+$showError = false;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  include 'partials/_dbconnect.php';
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  // $cpassword = $_POST["cpassword"];
+  if (empty($username) && empty($password) ){
+    $showError = "Invalid Credentials";
+  }else{
+    $sql  =  "SELECT *  FROM users WHERE username='$username' AND password='$password'";
+    $result =  mysqli_query($conn  , $sql);
+    $num  =  mysqli_num_rows($result);
+    if($num == 1){
+        $login = true;
+        session_start();
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $username;
+        header("location: welcome.php");
+      }
+  }
+  }
+
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <title>Sign Up</title>
+</head>
+
+<body>
+  <!-- <h1>Hello, world!</h1> -->
+  <?php require 'partials/_nav1.php' ?>
+  <?php
+  if ($login) {
+    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success you have Logged in!</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>';
+  }
+  if ($showError) {
+    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Error!</strong> '.$showError.'
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>';
+  }
+  ?>
+  <div class="container my-4">
+    <h3 class="text-center">Sign In to our Website</h3>
+    <!-- sign up form -->
+    <form action="/loginsystem/login.php" method="post">
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+            
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+       
+         
+        <button type="submit" class="btn btn-primary">Login</button>
+     </form>
+  </div>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ partials/_movies.php
@@ -0,0 +1,358 @@
+
+<div class="box">
+    <!-- mv 1 -->
+    <div class="modal fade" id="mv1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div>
+              <button type="button" style="border-radius:bold;" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body"  style="padding:0 ;">
+    <!-- movie url down here -->
+                <IFRAME SRC="https://sbflix.xyz/e/ki2sy44xe6te.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO PADDING:0 WIDTH=100% HEIGHT=360 allowfullscreen></IFRAME>
+            </div>
+            
+          </div>
+        </div>
+    </div>
+    <div class="box">
+            <div class="card" style="width: 18rem;">
+    <!-- image links -->
+                <img src="https://assets.thehansindia.com/h-upload/2022/06/10/1297027-nani-ante-sundaraniki-movie-review.webp" height="auto" class="card-img-top" alt="...">
+                <div class="card-body ">
+    <!-- movie name -->
+                    <h5 class="card-title">Ante Sundaraniki</h5>
+                    <input type="hidden" name="movie_name" value="Ante Sundaraniki" readonly>
+                    <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#mv1">
+                    Watch Online
+                  </button>
+                </div>
+            </div>
+            
+    </div>
+    <!-- mv 2 -->
+    <div class="modal fade" id="mv2" tabindex="-4" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div>
+              <button type="button" style="border-radius:bold;" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body" style="padding:0 ;">
+    <!-- movie url down here -->
+                <IFRAME SRC="https://sbflix.xyz/e/ruww16gtwwbz.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=100% HEIGHT=360 allowfullscreen></IFRAME>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    <div class="box">
+            <div class="card" style="width: 18rem;" >
+    <!-- image links -->
+                <img src="https://m.media-amazon.com/images/M/MV5BYjcyNDNkMzMtNWVjOC00ZTdjLTg1YTAtODMzMjMzZjJlNzEzXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg" height="auto" class="card-img-top" alt="...">
+                <div class="card-body">
+    <!-- movie name -->
+                    <h5 class="card-title">RRR (Telugu)</h5>
+                    <input type="hidden" name="movie_name" value="" readonly>
+                    <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#mv2">
+                    Watch Online
+                  </button>
+                </div>
+            </div>
+            
+    </div>
+    <!-- mv 3 -->
+    <div class="modal fade" id="mv3" tabindex="-4" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div>
+              <button type="button" style="border-radius:bold;" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body" style="padding:0 ;">
+    <!-- movie url down here -->
+                <IFRAME SRC="https://sbflix.xyz/e/x27u2prw0qdx.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=100% HEIGHT=360 allowfullscreen></IFRAME>
+            </div>
+            
+          </div>
+        </div>
+    </div>
+    <div class="box">
+            <div class="card" style="width: 18rem;">
+    <!-- image links -->
+                <img src="https://assets-in.bmscdn.com/content-buzz/2022/03/kgf-chapter-2-500x500-1.jpg" height="auto" class="card-img-top" alt="...">
+                <div class="card-body">
+    <!-- movie name -->
+                    <h5 class="card-title">KGF Chapter 2 (Telugu Dubbed)</h5>
+                    <input type="hidden" name="movie_name" value="" readonly>
+                    <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#mv3">
+                    Watch Online
+                  </button>
+                </div>
+            </div>
+            
+    </div>
+    <!-- mv 4 -->
+    <div class="modal fade" id="mv4" tabindex="-4" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div>
+              <button type="button" style="border-radius:bold;" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body" style="padding:0 ;">
+    <!-- movie url down here -->
+                <IFRAME SRC="https://sbflix.xyz/e/7m9l5n05ptcl.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=100% HEIGHT=360 allowfullscreen></IFRAME>
+            </div>
+            
+          </div>
+        </div>
+    </div>
+    <div class="box">
+            <div class="card" style="width: 18rem;">
+    <!-- image links -->
+                <img src="https://m.media-amazon.com/images/M/MV5BYzYyMGFlMDYtZmM0OS00ZDA0LTk5MmMtYzEwYTFlNzFiNjMzXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_FMjpg_UX1000_.jpg" height="auto" class="card-img-top" alt="...">
+                <div class="card-body">
+    <!-- movie name -->
+                    <h5 class="card-title">College Don (Telugu Dubbed)</h5>
+                    <input type="hidden" name="movie_name" value="" readonly>
+                    <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#mv4">
+                    Watch Online
+                  </button>
+                </div>
+            </div>
+            
+    </div>
+    <!-- mv 5 -->
+    <div class="modal fade" id="mv5" tabindex="-4" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div>
+              <button type="button" style="border-radius:bold;" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body" style="padding:0 ;">
+    <!-- movie url down here -->
+                <IFRAME SRC="https://sbflix.xyz/e/r9foik6448y9.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=100% HEIGHT=360 allowfullscreen></IFRAME>
+            </div>
+            
+          </div>
+        </div>
+    </div>
+    <div class="box">
+            <div class="card" style="width: 18rem;">
+    <!-- image links -->
+                <img src="https://www.filmibeat.com/fanimg/movie/19338/sarkaru-vaari-paata-photos-images-77590.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+    <!-- movie name -->
+                    <h5 class="card-title">Sarkaru Vaari Paata (Telugu)</h5>
+                    <input type="hidden" name="movie_name" value="" readonly>
+                    <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#mv5">
+                    Watch Online
+                  </button>
+                </div>
+            </div>
+            
+    </div>
+    <!-- mv 7 -->
+    <div class="modal fade" id="mv7" tabindex="-4" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div>
+              <button type="button" style="border-radius:bold;" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body" style="padding:0 ;">
+    <!-- movie url down here -->
+                <IFRAME SRC="https://sbflix.xyz/e/odmq628c9ldq.html/" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=100% HEIGHT=360 allowfullscreen></IFRAME>
+            </div>
+            
+          </div>
+        </div>
+    </div>
+    <div class="box">
+            <div class="card" style="width: 18rem;">
+    <!-- image links -->
+                <img src="https://m.media-amazon.com/images/M/MV5BNWM0ZGJlMzMtZmYwMi00NzI3LTgzMzMtNjMzNjliNDRmZmFlXkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_.jpg"  class="card-img-top" alt="...">
+                <div class="card-body">
+    <!-- movie name -->
+                    <h5 class="card-title">Doctor Strange in the Multiverse of Madness (Telugu Dubbed)</h5>
+                    <input type="hidden" name="movie_name" value="" readonly>
+                    <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#mv7">
+                    Watch Online
+                  </button>
+                </div>
+            </div>
+            
+    </div>
+    <!-- mv 8 -->
+    <div class="modal fade" id="mv8" tabindex="-4" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div>
+              <button type="button" style="border-radius:bold;" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body" style="padding:0 ;">
+    <!-- movie url down here -->
+                <IFRAME SRC="https://sbflix.xyz/e/saag6tk9xf4v.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=100% HEIGHT=360 allowfullscreen></IFRAME>
+            </div>
+            
+          </div>
+        </div>
+    </div>
+    <div class="box">
+            <div class="card" style="width: 18rem;">
+    <!-- image links -->
+                <img src="https://static.toiimg.com/photo/msid-92268745/92268745.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+    <!-- movie name -->
+                    <h5 class="card-title">Virata Parvam (Telugu)</h5>
+                    <input type="hidden" name="movie_name" value="" readonly>
+                    <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#mv8">
+                    Watch Online
+                  </button>
+                </div>
+            </div>
+            
+    </div><!-- mv 9 -->
+    <div class="modal fade" id="mv9" tabindex="-4" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div>
+              <button type="button" style="border-radius:bold;" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body" style="padding:0 ;">
+    <!-- movie url down here -->
+                <IFRAME SRC="https://sbflix.xyz/e/a4q09fh3v87p.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=100% HEIGHT=360 allowfullscreen></IFRAME>
+            </div>
+            
+          </div>
+        </div>
+    </div>
+    <div class="box">
+            <div class="card" style="width: 18rem;">
+    <!-- image links -->
+                <img src="https://sklktcdnems01.cdnsrv.jio.com/c.saavncdn.com/984/Major-Telugu-Telugu-2022-20220606095932-500x500.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+    <!-- movie name -->
+                    <h5 class="card-title">Major (Telugu)</h5>
+                    <input type="hidden" name="movie_name" value="" readonly>
+                    <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#mv9">
+                    Watch Online
+                  </button>
+                </div>
+            </div>
+            
+    </div>
+    <!-- mv 10 -->
+    <div class="modal fade" id="mv10" tabindex="-4" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div>
+              <button type="button" style="border-radius:bold;" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body" style="padding:0 ;">
+    <!-- movie url down here -->
+                <IFRAME SRC="https://sbflix.xyz/e/o81jc9om7qsj.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=100% HEIGHT=360 allowfullscreen></IFRAME>
+            </div>
+            
+          </div>
+        </div>
+    </div>
+    <div class="box">
+            <div class="card" style="width: 18rem;">
+    <!-- image links -->
+                <img src="https://www.filmibeat.com/img/220x80x275/popcorn/movie_posters/pakkacommercial-20211016173855-19808.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+    <!-- movie name -->
+                    <h5 class="card-title">Pakka Commercial (Telugu)</h5>
+                    <input type="hidden" name="movie_name" value="" readonly>
+                    <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#mv10">
+                    Watch Online
+                  </button>
+                </div>
+            </div>
+            
+    </div>
+<!-- mv 11 -->
+
+    <!-- mv 12 -->
+    <div class="modal fade" id="mv12" tabindex="-4" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div>
+              <button type="button" style="border-radius:bold;" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body" style="padding:0 ;">
+    <!-- movie url down here -->
+                <IFRAME SRC="https://sbflix.xyz/e/7ujtmnxgwh9t.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=100% HEIGHT=360 allowfullscreen></IFRAME>
+            </div>
+            
+          </div>
+        </div>
+    </div>
+    <div class="box">
+            <div class="card" style="width: 18rem;">
+    <!-- image links -->
+                <img src="https://m.media-amazon.com/images/M/MV5BYmMxZWRiMTgtZjM0Ny00NDQxLWIxYWQtZDdlNDNkOTEzYTdlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+    <!-- movie name -->
+                    <h5 class="card-title">Thor: Love and Thunder (Telugu Dubbed)</h5>
+                    <input type="hidden" name="movie_name" value="" readonly>
+                    <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#mv12">
+                    Watch Online
+                  </button>
+                </div>
+            </div>
+            
+    </div>
+    <!-- mv 13 -->
+    <div class="modal fade" id="mv13" tabindex="-4" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div>
+              <button type="button" style="border-radius:bold;" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body" style="padding:0 ;">
+    <!-- movie url down here -->
+                <IFRAME SRC="https://sbflix.xyz/e/0a07m35tmz9y.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=100% HEIGHT=360 allowfullscreen></IFRAME>
+            </div>
+            
+          </div>
+        </div>
+    </div>
+    <div class="box">
+            <div class="card" style="width: 18rem;">
+    <!-- image links -->
+                <img src="https://igimages.gumlet.io/tamil/home/vikram-censor24052022m.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+    <!-- movie name -->
+                    <h5 class="card-title">Vikram (Telugu Dubbed)</h5>
+                    <input type="hidden" name="movie_name" value="" readonly>
+                    <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#mv13">
+                    Watch Online
+                  </button>
+                </div>
+            </div>
+            
+    </div>
+    </div>
+</div>
+</div>
